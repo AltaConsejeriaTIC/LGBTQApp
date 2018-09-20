b4ae530873b9215b093ccf8800b98173e3338e39
EP <milopenaloza@gmail.com>
2018-09-20 15:54:11 -0500

    added slide funcionality to arrows in first carrousel

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+
+  @ViewChild('slides') slides: Slides;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  prevSlide() {
+    this.slides.slidePrev();
+  }
+
+  nextSlide() {
+    this.slides.slideNext();
   }
 
 }
