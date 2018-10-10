b5d1b9fc1fcd88b18923f8736453fc21640f8d3b
EP <milopenaloza@gmail.com>
2018-10-09 19:26:11 -0500

    Added more news in list

--- src/pages/listado/listado.ts
+++ src/pages/listado/listado.ts
@@ -22,6 +22,11 @@ export class ListadoPage {
   public rect3: any;
   public rect4: any;
 
+  public pos1: any;
+  public pos2: any;
+  public pos3: any;
+  public pos4: any;
+
   newsTitle: string[] = ['Aguanilé','Ciclo rosa 2018 Muestras audiovisuales','Noticia 3','Noticia 4'];
   newsDate: string[] = ['Agosto','Septiembre','Octubre','Noviembre'];
   newsDuration: string[] = ['Del 28 de Marzo al 30 de Marzo de 2018','Del 28 de Marzo al 30 de Marzo de 2018','Del 28 de Marzo al 30 de Marzo de 2018','Del 28 de Marzo al 30 de Marzo de 2018']
@@ -33,13 +38,13 @@ export class ListadoPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListadoPage');
-    if(this.evento){
+
       this.content.ionScroll.subscribe(() => {
       this.changeMonthTitle();
+      this.changeMonthTitleNews();
       });
-    }
-
   }
+
 
   changeSectionEvents() {
     this.evento = true;
@@ -60,10 +65,11 @@ export class ListadoPage {
   }
 
   changeMonthTitle(){
-      this.rect1 = document.getElementById("new1").getBoundingClientRect();
-      this.rect2 = document.getElementById("new2").getBoundingClientRect();
-      this.rect3 = document.getElementById("new3").getBoundingClientRect();
-      this.rect4 = document.getElementById("new4").getBoundingClientRect();
+    if(this.evento){
+      this.rect1 = document.getElementById("article1").getBoundingClientRect();
+      this.rect2 = document.getElementById("article2").getBoundingClientRect();
+      this.rect3 = document.getElementById("article3").getBoundingClientRect();
+      this.rect4 = document.getElementById("article4").getBoundingClientRect();
 
           if(this.rect1.top > 420 && this.rect1.top < 600){
               this.monthTitle=this.newsDate[0];
@@ -78,5 +84,36 @@ export class ListadoPage {
               this.monthTitle=this.newsDate[3];
           }
     this.ref.detectChanges();
+    }
+    else{
+      return;
+    }
   }
+
+  changeMonthTitleNews(){
+      if(this.noticia){
+        this.pos1 = document.getElementById("new1").getBoundingClientRect();
+        this.pos2 = document.getElementById("new2").getBoundingClientRect();
+        this.pos3 = document.getElementById("new3").getBoundingClientRect();
+        this.pos4 = document.getElementById("new4").getBoundingClientRect();
+
+        console.log(this.pos1.top, this.pos2.top, this.pos3.top, this.pos4.top);
+
+            if(this.pos1.top > 420 && this.pos1.top < 600){
+                this.monthTitle=this.newsDate[0];
+            }
+            if(this.pos2.top > 420 && this.pos2.top < 600){
+                this.monthTitle=this.newsDate[1];
+            }
+            if(this.pos3.top > 420 && this.pos3.top < 600){
+                this.monthTitle=this.newsDate[2];
+            }
+            if(this.pos4.top > 420 && this.pos4.top < 600){
+                this.monthTitle=this.newsDate[3];
+            }
+      this.ref.detectChanges();
+    }
+      }
+
+
 }
