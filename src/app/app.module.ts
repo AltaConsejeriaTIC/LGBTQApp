ff1258411902a6fe1edc6464e0fdd18d72080cf6
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2018-11-07 18:41:49 -0500

    fix background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { Deeplinks } from '@ionic-native/deeplinks'
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ModalMenuPage } from '../pages/modal-menu/modal-menu';
-import { ModalSpeedDialPage } from '../pages/modal-speed-dial/modal-speed-dial';
 import { ListadoPage } from '../pages/listado/listado';
 import { DerechoPage } from '../pages/derecho/derecho';
 import { DenunciaPage } from '../pages/denuncia/denuncia';
@@ -55,7 +54,6 @@ import { ContentDetailModule } from '../pages/content-detail/content-detail.modu
     MyApp,
     HomePage,
     ModalMenuPage,
-    ModalSpeedDialPage,
     ListadoPage,
     DerechoPage,
     DenunciaPage,
@@ -87,7 +85,6 @@ import { ContentDetailModule } from '../pages/content-detail/content-detail.modu
     MyApp,
     HomePage,
     ModalMenuPage,
-    ModalSpeedDialPage,
     ListadoPage,
     DerechoPage,
     DenunciaPage,
