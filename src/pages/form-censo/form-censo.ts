8c303d72d4ffc73d9ecfe86986b872cf72e388da
EP <milopenaloza@gmail.com>
2018-11-07 18:52:27 -0500

    added validations

--- src/pages/form-censo/form-censo.ts
+++ src/pages/form-censo/form-censo.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 
 @Component({
@@ -12,19 +12,59 @@ export class FormCensoPage {
   credentialsForm: FormGroup;
   modalWindow: boolean = false;
 
+  validation_messages = {
+'email': [
+		{ type: 'required', message: 'El correo es requerido.' },
+		{ type: 'pattern', message: 'El formato no es válido' }
+	],
+  'documentType': [
+		{ type: 'required', message: 'Debe escoger un tipo de documento' }
+	],
+  'documentNumber': [
+		{ type: 'required', message: 'El número de documento es requerido' }
+	],
+  'firstName': [
+		{ type: 'required', message: 'El nombre es requerido.' }
+	],
+  'lastName': [
+		{ type: 'required', message: 'El apellido es requerido.' }
+	],
+  'address': [
+		{ type: 'required', message: 'La dirección es requerida.' }
+	],
+  'phone': [
+		{ type: 'required', message: 'El teléfono es requerido.' }
+	],
+  'gender': [
+		{ type: 'required', message: 'El sexo de nacimiento es requerido.' }
+	],
+  'orientation': [
+		{ type: 'required', message: 'La orientación sexual es requerida.' }
+	],
+  'identity': [
+		{ type: 'required', message: 'El género es requerido.' }
+	],
+  'age': [
+		{ type: 'required', message: 'La edad es requerida.' }
+	]
+}
+
   constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
     this.credentialsForm = this.formBuilder.group({
-      email: [''],
-      documentType: [''],
-      documentNumber: [''],
-      firstName: [''],
-      lastName: [''],
-      address: [''],
-      phone: [''],
-      gender: [''],
-      orientation: [''],
-      identity: [''],
-      age: ['']
+      email: new FormControl ('', Validators.compose([
+                              		Validators.required,
+                              		Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+                              	])),
+      documentType: new FormControl ('', Validators.required),
+      documentNumber: new FormControl ('', Validators.required),
+      firstName: new FormControl ('', Validators.required),
+      lastName: new FormControl ('', Validators.required),
+      address: new FormControl ('', Validators.required),
+      phone: new FormControl ('', Validators.required),
+      gender: new FormControl ('', Validators.required),
+      orientation: new FormControl ('', Validators.required),
+      identity: new FormControl ('', Validators.required),
+      age: new FormControl ('', Validators.required)
 
     });
   }
