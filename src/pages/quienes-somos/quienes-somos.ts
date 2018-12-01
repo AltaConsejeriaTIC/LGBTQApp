48d7f411d71aa911da674cd1fb70810451f1f817
EP <milopenaloza@gmail.com>
2018-11-30 23:28:29 -0500

    social networks open in outside window

--- src/pages/quienes-somos/quienes-somos.ts
+++ src/pages/quienes-somos/quienes-somos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 /**
  * Generated class for the QuienesSomosPage page.
@@ -13,9 +14,21 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'quienes-somos.html'
 })
 export class QuienesSomosPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private iab: InAppBrowser) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad QuienesSomosPage');
+  }
+  goToYoutube() {
+    this.iab.create('https://www.youtube.com/channel/UCG8WBCPdvPNduZiHrc2gKNQ', '_system');
+  }
+  goToTwitter() {
+    this.iab.create('https://twitter.com/sepuedeser', '_system');
+  }
+  goToInstagram() {
+    this.iab.create('https://www.instagram.com/sepuedeser/', '_system');
+  }
+  goToFacebook() {
+    this.iab.create('https://www.facebook.com/enbogotasepuedeser/', '_system');
   }
 }
