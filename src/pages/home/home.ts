5583950b03c37b64ff5944963145d87664683e5f
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2018-12-12 18:31:00 -0500

    fix carrusel la venganza

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,7 +15,7 @@ import { AndroidPermissions } from '@ionic-native/android-permissions';
 import { Platform } from 'ionic-angular';
 import {ServerConfig} from "../../../config/server";
 import {EventProvider} from "../../providers/event/event";
-
+import { DetailContentService } from '../../services/detail-content.service';
 
 @Component({
   selector: 'page-home',
@@ -38,6 +38,7 @@ export class HomePage {
     private sms: SMS,
     private callNumber: CallNumber,
     private storage: Storage,
+    private detailService: DetailContentService,
     private androidPermissions: AndroidPermissions,
     public plt: Platform,
     public eventService: EventProvider
@@ -59,6 +60,7 @@ export class HomePage {
         highlights.forEach((highlight) => {
           this.eventService.get(this.api, highlight.section == "event" ? "events" : "news", highlight.section_id).subscribe(
             (response) => {
+              response.isEvent = highlight.section == "event";
               this.slidesElementes.push(response);
               console.log(this.slidesElementes)
             },
@@ -333,6 +335,7 @@ export class HomePage {
 
   goToDetails() {
     let event = this.slidesElementes[(this.slides.clickedIndex-1)%(this.slides.length()-2)];
+    this.detailService.setEvento(event.isEvent);
     this.navCtrl.push('content', { id: event.id , data: event}).catch(err => {
       console.error("Error en goToDetails", err, err.stack);
     });
